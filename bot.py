5b8036a8e839ecda283bd35fb44394f790083a72
Abbi <127574320+AbbysTheWorld@users.noreply.github.com>
2024-02-14 15:38:36 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,9 +16,9 @@ chave_api = os.environ.get("API_GPT")
 openai.api_key = chave_api
 
 options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-options.add_argument('--enable-chrome-browser-cloud-management')
-#options.add_argument('--headless=new')
+#options.add_experimental_option("detach", True)
+#options.add_argument('--enable-chrome-browser-cloud-management')
+options.add_argument('--headless=new')
 
 intents = discord.Intents.default()
 intents.message_content = True
